ffdcfc3d7308d2b5d8ab56bdcce8344a22196a74
Richard Todd <rwtodd@users.noreply.github.com>
2020-04-14 23:04:52 -0500

    Adding functions to facilitate exploration. UI is next.
    I am building functions I know the UI will need to get its
    job done, like `recenter!` with a scaling option.  I'm
    pleasantly surprised with how small the code is turning
    out to be.  Clojure can be a real pleasure sometimes.

--- src/org_rwtodd/fractals/core.clj
+++ src/org_rwtodd/fractals/core.clj
@@ -9,11 +9,10 @@
 (def starter-spec
   "Default starting point for fractal exploration"
   {
-   :fractal "(algo/->Mandelbrot 32 4.0)"
-   :colors
- "(colors/vectorize-scheme
- (colors/combine-schemes 0
-                         (colors/gradient-scheme 255 0xff0000 0xddff00 0x0000ff)))"
+   :in-fractal "(algo/->Mandelbrot 32 4.0)"
+   :in-scheme
+ "(colors/combine-schemes 0
+                         (colors/gradient-scheme 255 0xff0000 0xddff00 0x0000ff))"
    :center [-0.5 0.0]
    :size [1.0 1.0]
    :image-size [300 300]
@@ -22,11 +21,16 @@
 (defn- evaluate-state
   "fill out the application state by evaluating the inputs."
   [st]
-  (let [[sx sy] (:image-size st)]
+  (let [[sx sy] (:image-size st)
+        existing-img (:image st)]
     (assoc st
-           :algorithm (eval (read-string (:fractal st)))
-           :scheme    (eval (read-string (:colors st)))
-           :image     (BufferedImage. sx sy BufferedImage/TYPE_INT_RGB))))
+           :fractal (eval (read-string (:in-fractal st)))
+           :scheme  (colors/vectorize-scheme (eval (read-string (:in-scheme st))))
+           :image   (if (and existing-img
+                             (= (.getWidth existing-img) sx)
+                             (= (.getHeight existing-img) sy))
+                      existing-img
+                      (BufferedImage. sx sy BufferedImage/TYPE_INT_RGB)))))
 
 (def app-state (atom (evaluate-state starter-spec)))
 
@@ -35,10 +39,31 @@
   (let [st @app-state
         [cx cy]    (:center st)
         [szx szy]  (:size   st)]
-    (algo/fill-image (:image st) (:algorithm st) (:scheme st)
+    (algo/fill-image (:image st) (:fractal st) (:scheme st)
                      {:xmin (- cx szx) :xmax (+ cx szx)
                       :ymin (- cy szy) :ymax (+ cy szy)})))
-        
+
+
+(defn recenter!
+  "Adjust the global state for a new center `x` and `y`, with optional
+  rescaling by `scale`"
+  ([x y]
+   (swap! app-state assoc :center [x y]))
+  ([x y scale]
+   (swap! app-state
+          (fn [s]
+            (assoc s
+                   :center [x y],
+                   :size (let [[a b] (:size s)]
+                           [(* scale a) (* scale b)]))))))
+  
+(defn change-inputs!
+  "Update the global state's existing keys with any provided."
+  [& kvs]
+  (swap! app-state
+         (fn [s]
+           (evaluate-state (apply assoc s kvs)))))
+    
 (defn generate-frame
   "Start the main frame--must be called from swing thread"
   []
